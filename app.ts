996d6078ebe9ebcdfbdcb1a35731707560e65726
Dmitrii <dimonanos@gmail.com>
2021-01-16 00:56:55 +0300

    sync

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import config from 'config';
-import path from 'path'
+import path from 'path';
 
 import { authRouter, budgetRouter } from './routes';
 // import budget from './routes/budget.routes'
@@ -14,11 +14,11 @@ app.use('/api/auth', authRouter);
 app.use('/api/budget', budgetRouter);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+	app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	})
+	app.get('*', (req: Request, res: Response) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 const PORT: number = config.get('port') || 8000;
