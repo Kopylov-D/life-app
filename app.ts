52caaa5f9d75a9cc8cbb7b8507d7208af78dff78
Dmitrii <dimonanos@gmail.com>
2021-01-16 21:17:26 +0300

    finish files to deploy

--- app.ts
+++ app.ts
@@ -1,10 +1,9 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import config from 'config';
 import path from 'path';
 
 import { authRouter, budgetRouter } from './routes';
-// import budget from './routes/budget.routes'
 
 const app: express.Application = express();
 
@@ -13,24 +12,20 @@ app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/budget', budgetRouter);
 
-let PORT: string | number = config.get('port') || 8008
+let PORT: string | number = config.get('port') || 8008;
+const uri: string = config.get('mongoUri');
 
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
 
-	PORT = process.env.PORT || 8008
-	// app.use(express.static('client/build'));
-
-	// app.get('*', (req: Request, res: Response) => {
-	// 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	// });
-
 	app.get('*', (req: Request, res: Response) => {
-		res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+		res.sendFile(
+			path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+		);
 	});
-}
 
-const uri: string = config.get('mongoUri');
+	PORT = process.env.PORT || 8008;
+}
 
 async function start() {
 	try {
@@ -46,11 +41,6 @@ async function start() {
 		app.listen(PORT, () =>
 			console.log(`App has been started on port ${PORT}...`)
 		);
-		app.use((req: Request, res: Response) => {
-			if (req.url === '/api/budeget/info') {
-				res.status(204).json('all good');
-			}
-		});
 	} catch (e) {
 		console.log('Server error', e);
 		process.exit(1);
